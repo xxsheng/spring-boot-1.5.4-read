990f4e1b10085f5f088da704276723e6bf3c23dc
xxsheng <1558281773@qq.com>
2020-09-30 10:42:47 +0800

    第十四章 springboot源码分析  14.4 完结

--- spring-boot-starters/spring-boot-starter-xxq/src/main/java/org/springboot/framework/service/HelloService.java
+++ spring-boot-starters/spring-boot-starter-xxq/src/main/java/org/springboot/framework/service/HelloService.java
@@ -1,5 +1,13 @@
 package org.springboot.framework.service;
 
+import org.springframework.context.annotation.Bean;
+
 public interface HelloService {
 	public String sayHello();
+
+	// 验证可以注入
+	@Bean
+	default public Object get() {
+		return new Object();
+	}
 }
