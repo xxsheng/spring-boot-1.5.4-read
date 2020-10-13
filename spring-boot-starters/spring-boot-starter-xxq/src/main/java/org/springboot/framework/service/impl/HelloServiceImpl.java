30ab12f31c1aba80fe17e483af4d336608cc9038
xxsheng <1558281773@qq.com>
2020-10-13 17:41:06 +0800

    第十四章 springboot源码分析  14.6属性自动化配置 000

--- spring-boot-starters/spring-boot-starter-xxq/src/main/java/org/springboot/framework/service/impl/HelloServiceImpl.java
+++ spring-boot-starters/spring-boot-starter-xxq/src/main/java/org/springboot/framework/service/impl/HelloServiceImpl.java
@@ -1,12 +1,25 @@
 package org.springboot.framework.service.impl;
 
 import org.springboot.framework.service.HelloService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloServiceImpl implements HelloService {
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	@Value("${study.testStr}")
+	private String test;
+
 	@Override
 	public String sayHello() {
-		return "hello!!";
+		return "hello!!" + test;
 	}
 }
